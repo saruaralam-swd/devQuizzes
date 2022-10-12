6ae0ed4f2348cbad42728ea1c3976d72ad45b4ac
saruaralam-swd <saruaralam2001@gmail.com>
2022-10-12 19:41:35 +0600

    solve some problem about question

--- src/Component/Question/Question.js
+++ src/Component/Question/Question.js
@@ -6,13 +6,14 @@ import './Question.css'
 const Question = ({ question }) => {
   const { id, options, correctAnswer } = question;
 
-  const newQuestion = (question.question.replace( /(<([^>]+)>)/ig, ''));
-
-  console.log();
+  const Question = (question.question.replace( /(<([^>]+)>)/ig, ''));
 
   const clickHandle = (event) => {
-    const newValue = (event.target.innerText).replace(/\s+/g, '').trim();
-    const newCorrectAnswer = (correctAnswer).replace(/\s+/g, '').trim();
+    const newValue = event.target.innerText.trim();
+    const newCorrectAnswer = (correctAnswer).replace(/\s+/g, ' ').trim();
+
+    console.log(newValue);
+    console.log(newCorrectAnswer);
 
     if (newValue === newCorrectAnswer) {
       (event.target.classList.add('bg-green-300'));
@@ -26,7 +27,6 @@ const Question = ({ question }) => {
     }
 
     // const result = (options.filter(text => (text.replace(/\s+/g, '').trim()) !== newCorrectAnswer));
-
     // const parent = (event.target.parentNode.parentNode);
     // const target = (event.target);
     // console.log(parent.nextSibling);
@@ -39,14 +39,14 @@ const Question = ({ question }) => {
       <div className=' rounded-md ring-2 shadow-2xl my-10'>
         <div className='  rounded-lg p-10'>
           <div className='flex gap-5 items-start justify-between'>
-            <h2 className='text-2xl font-semibold text-center mb-10'>{newQuestion}</h2>
+            <h2 className='text-2xl font-semibold text-center mb-10'>{Question}</h2>
             <button onClick={eyeHandle} className='hover:text-indigo-600'><EyeIcon className="h-6 w-6 " /></button>
           </div>
           <ul className='text-xl space-y-5' id={id}>
             {
               options.map((option, idx) =>
                 <div key={idx} >
-                  <button onClick={clickHandle} className=" bg-slate-300 text-left rounded-md border p-2 w-full">
+                  <button onClick={clickHandle} className=" bg-slate-300 text-left text-lg rounded-md border p-2 w-full">
                     {option}
                   </button>
                 </div>
